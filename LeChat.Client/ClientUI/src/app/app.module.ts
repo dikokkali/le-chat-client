81a7bea7798183839b96dd1d0981af6f4d741ebb
Stratos Kokkaliaroglou <stratoskokka@gmail.com>
2022-06-05 14:56:52 +0300

    Basic chat window UI, Material installed

--- LeChat.Client/ClientUI/src/app/app.module.ts
+++ LeChat.Client/ClientUI/src/app/app.module.ts
@@ -5,6 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChatWindowComponent } from './shared/components/chat-window/chat-window.component';
 import { TextBubbleComponent } from './shared/components/text-bubble/text-bubble.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSliderModule } from '@angular/material/slider';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +21,12 @@ import { TextBubbleComponent } from './shared/components/text-bubble/text-bubble
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
